900ff9ddf48c2a68881ceaca9a6a6a9abfe63d11
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-08-29 18:01:24 +0900

    ルーターをアプリケーションに登録

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import store from './store.js'
-import router from './router'
+import router from './router/index.js'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  store,
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
