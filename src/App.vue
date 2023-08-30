962287882e199a7fb8537ed4629185ac69a5540c
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-08-30 10:46:46 +0900

    微調整

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <nav>
-      <router-link to="/">Home</router-link>
+      <router-link to="/" exact>Home</router-link>
       <router-link to="/product">商品情報</router-link>
     </nav>
      <!-- ここにパスと一致したコンポーネントが埋め込まれる -->
