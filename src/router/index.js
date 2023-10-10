bf27f27fc51c76025629614dd3b3087c785f128b
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-10-10 13:31:54 +0900

    スクロールの振る舞いを操作する

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,17 @@ const router = new VueRouter({
         }
       ]
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+  }
 })
 
 router.beforeEach((to, from, next) => {
